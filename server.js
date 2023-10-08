9f8453aadb575c4300985468200b85329eec1f8a
Bryan Fernando <bryanfernandoks11@gmail.com>
2023-10-08 23:33:41 +0700

    rename env key to lowercase

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /* Service(s) */
 const sendd_mail = require("./services/sendmail.js");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.port || 5000;
 
 // Server route(s)
 // Index route
