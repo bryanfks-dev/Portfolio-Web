bcd3deed6736b937b84a92ca8d48710bda1c67df
Bryan Fernando <bryanfernandoks11@gmail.com>
2023-10-09 08:13:20 +0700

    add port to sendmail.js

--- services/sendmail.js
+++ services/sendmail.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const send_mail = async function (name, email, message) {
     // Set transporter
     const transporter = nodemailer.createTransport({
+        port: 456,
         service: 'gmail',
         auth: {
             user: 'bryanfernandoks11@gmail.com',
