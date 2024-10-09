aaff9e23faab21a49ce6efd93718ce7095c706e6
bryanfks-dev <bryanfernandoks11@gmail.com>
2024-10-09 18:38:51 +0700

    update stats section

--- src/app/home/stats.tsx
+++ src/app/home/stats.tsx
@@ -3,7 +3,7 @@
 import BlockCodeSolidIcon from "@/presentation/icons/blockCodeSolid";
 import HeartSolidIcon from "@/presentation/icons/happySolid";
 import PuzzlePieceSolidIcon from "@/presentation/icons/puzzlePieceSolid";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 /**
  * StatsSection is a section that displays my current statistics.
@@ -35,41 +35,41 @@ export default function StatsSection(): JSX.Element {
   return (
     <section
       id="stats"
-      className="flex flex-col flex-wrap gap-8 bg-primary px-4 py-8 *:flex-1 md:grid md:p-8 lg:flex-row lg:p-16"
+      className="flex flex-col flex-wrap gap-4 bg-primary px-4 py-8 *:flex-1 md:grid md:grid-flow-row md:grid-cols-2 md:gap-8 md:p-8 md:py-16 lg:grid-cols-3 lg:flex-row lg:gap-16 lg:px-16 lg:py-20"
     >
-      <div className="flex flex-col gap-5 *:flex-1 md:flex-row">
-        <div className="flex h-96 min-h-72 flex-col justify-end gap-5 bg-secondary p-8 text-text-secondary lg:p-12">
-          <PuzzlePieceSolidIcon
-            className="size-20 text-secondary brightness-90 md:hidden"
-            strokeWidth={1.5}
-          />
-
-          <span className="flex gap-5 text-6xl font-bold md:flex-col md:text-7xl lg:text-8xl">
-            {yearsOfExperience}+
-            <p className="text-2xl font-medium">YEARS OF EXPERIENCES.</p>
-          </span>
-        </div>
-
-        <div className="flex h-96 min-h-72 flex-col justify-end gap-5 bg-secondary p-8 text-text-secondary lg:p-12">
-          <BlockCodeSolidIcon
-            className="size-20 text-secondary brightness-90 md:hidden"
-            strokeWidth={1.5}
-          />
-
-          <span className="flex gap-5 text-6xl font-bold md:flex-col md:text-7xl lg:text-8xl">
-            {projectsCompletedCount}
-            <p className="text-2xl font-medium">PROJECTS COMPLETED.</p>
-          </span>
-        </div>
-      </div>
-
-      <div className="flex h-96 min-h-72 flex-col justify-end gap-5 bg-secondary p-8 text-text-secondary lg:p-12">
-        <HeartSolidIcon
-          className="size-20 text-secondary brightness-90 md:hidden"
+      <div className="flex h-96 min-h-72 flex-col justify-end gap-5 rounded-3xl bg-secondary p-8 text-text-secondary md:h-72 md:items-center md:justify-center md:p-2 lg:p-12">
+        <PuzzlePieceSolidIcon
+          className="size-24 text-secondary brightness-90 md:hidden"
           strokeWidth={1.5}
         />
 
-        <span className="flex gap-5 text-6xl font-bold md:flex-col md:text-7xl lg:text-8xl">
+        <span className="flex select-none items-end gap-5 text-6xl font-bold md:flex-col md:items-center md:justify-center md:gap-2 md:text-7xl lg:text-8xl">
+          {yearsOfExperience}+
+          <p className="text-2xl font-medium md:text-center">
+            YEARS OF EXPERIENCES.
+          </p>
+        </span>
+      </div>
+
+      <div className="flex h-96 min-h-72 flex-col justify-end gap-5 rounded-3xl bg-secondary p-8 text-text-secondary md:h-72 md:justify-center lg:p-12">
+        <BlockCodeSolidIcon
+          className="size-24 text-secondary brightness-90 md:hidden"
+          strokeWidth={1.5}
+        />
+
+        <span className="flex select-none items-end gap-5 text-6xl font-bold md:flex-col md:items-center md:gap-2 md:text-7xl lg:text-8xl">
+          {projectsCompletedCount}
+          <p className="text-2xl font-medium">PROJECTS COMPLETED.</p>
+        </span>
+      </div>
+
+      <div className="flex h-96 min-h-72 flex-col justify-end gap-5 rounded-3xl bg-secondary p-8 text-text-secondary md:col-span-2 md:h-72 md:justify-center lg:col-span-1 lg:p-12">
+        <HeartSolidIcon
+          className="size-24 text-secondary brightness-90 md:hidden"
+          strokeWidth={1.5}
+        />
+
+        <span className="flex select-none items-end gap-5 text-6xl font-bold md:flex-col md:items-center md:gap-2 md:text-7xl lg:text-8xl">
           {happyClientsCount}
           <p className="text-2xl font-medium">HAPPY CLIENTS.</p>
         </span>
